0b9867daea15282211ccc9310b9f56dad8c4112f
kha ata <ahkman.100@gmail.com>
2025-05-14 16:51:47 +0700

    commit slot1

---
+++ slot1/classdemo/vdone/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>chao bạn</h1>
+    <h2>aloo</h2>
+    </div>
+  );
+}
+<h1>loo</h1>
+export default App;
